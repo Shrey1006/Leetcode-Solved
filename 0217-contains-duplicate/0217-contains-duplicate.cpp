6bdb1ceb831e67859412b3b6629e2fab61bcb042
Shreyansh Mojidra <shreymojidra06@gmail.com>
2026-09-14 11:57:58 +0530

    Time: 71 ms (60.17%), Space: 111.2 MB (62.96%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> mp;
+        for(int i=0;i<nums.size();i++){
+            if(mp.count(nums[i])) return true;
+            mp.insert(nums[i]);
+        }
+        return false;
+    }
+};
